49f82a87182684141f3681ab6d736522d0bb9633
kaikeventura <kaike.gnr@hotmail.com>
2020-08-21 02:03:31 -0300

    First commit

---
+++ src/main/java/br/com/kaikeventura/restmongodb/RestmongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.kaikeventura.restmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kaikeventura/restmongodb/RestmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kaikeventura.restmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
